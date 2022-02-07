4c127bfc7907c4c3090a0ec9631ab1f862bc0702
Kendall McGree <mcgreekendall@gmail.com>
2022-02-07 06:43:21 -0800

    Initial commit

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,33 @@
+function createLibrary(name) {
+  var library = {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+    },
+    // isOpen: true,
+    // capacity: 50,
+  };
+  return library;
+}
 
+function addBook(library, book) {
+  //find the shelf to add it
+  var shelfForBook = library.shelves[book.genre];
+  shelfForBook.push(book);
+  //put the book onto the shelf once we find it
+  //console.log(book.genre);
+}
+//find a book to unshelf
+//add book to checkout
+//print message
+function checkoutBook(library, book) {
+  for (var i = 0; i < library.length; i++) {}
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook,
 };
